f6877da3970d14e160dcd3de768535d6b1f210ec
KAWANO Yohei <yohei.kawano@gmail.com>
2014-09-27 03:37:37 +0000

    login_logにインデックス追加

--- init.sh
+++ init.sh
@@ -11,3 +11,6 @@ mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
+
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} -e"alter table login_log add index(user_id)"
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} -e"alter table login_log add index(ip)"
